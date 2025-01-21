652a90a53add584fcfe4d0fb7f570107737d994b
JonathamCarvalho <jonatham.carvalho@gmail.com>
2025-01-20 21:21:44 -0300

    projeto-jokenpô

---
+++ script.js
@@ -0,0 +1,58 @@
+const result = document.querySelector(".result")
+const HumanScore = document.querySelector(".human-score")
+const iaScore = document.querySelector("#ia-score")
+const resultIa = document.querySelector("#result-ia")
+
+let playerScore = 0
+let MachineScore = 0
+
+function playHuman(humanChoice) {
+   
+    playTheGame(humanChoice, playIA())
+
+}
+
+function playIA(iaChoice) {
+
+    const choices = ['pedra', 'papel', 'tesoura']
+
+    const randomNumber = Math.floor(Math.random() * 3)
+    return choices[randomNumber]
+}
+
+
+function playTheGame(human, ia){
+    console.log("Humano:" + human + "Ia:" + ia)
+
+
+    if(human === ia){
+        
+        result.innerHTML = "Empatou &#x1F605"
+    }
+    else if(human === 'papel' && ia === 'pedra' || 
+            human === 'pedra' && ia === 'tesoura' ||
+            human === 'tesoura' && ia === 'papel' ){
+        
+        playerScore++
+        HumanScore.innerHTML = playerScore      
+
+        result.innerHTML = "Você ganhou &#x1F60E"
+
+       
+    }
+    else{
+
+        MachineScore++
+        iaScore.innerHTML = MachineScore
+
+         result.innerHTML = "IA ganhou &#x1F62D"
+    }
+
+    resultIa.innerHTML = ia 
+
+}
+
+
+
+
+
